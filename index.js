520522d64d4be41128fae29b42c0d2a58760e2cc
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-03 00:21:38 +0530

    opeartors Precedences

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-// BitWise Operators
-
-
-//Human code -> Machine code (0,1)
-
-// 1 -> 00000001 -> 1
-// 2 -> 00000002 -> 2
-// 3 -> 00000011 -> 3
-//console.log(1 | 2) // bitwise OR
-//console.log(1 & 1) // bitwise AND
-
-// example 
-// R -> 00000100 -> Read
-// w -> 00000010 -> Write
-// E -> 00000001 -> Execute
-
- const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-
-myPermission = myPermission | writePermission | readPermission;
-// use Ternary operator
-let message = (myPermission & readPermission) ? "yes" : "No";
-console.log(message);
- 
+// Operators Precedence 
+let x = (2 + 5) * 10;  // 70
+let y = 2 + 5 * 10; // 52 precedence type
+console.log(y);
+console.log(x);
