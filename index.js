ca0da0fb04ee59edb3f0fe5f096e0c7d70074c7b
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-04 23:53:23 +0530

    while loop

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// for loop
-for (let i = 0; i <= 10; i++){
-    if (i % 2 !== 0) {
-        console.log("Add number #" + i);
+// While Loop
+let i = 1;
+while (i <= 20) {
+    if (i %2 == 0) {
+        console.log("even Number #" + i);
         
     }
+    i++;
 }
