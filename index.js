228a34fbfdf5f1f3a65da7a69e4ddcb9a88f198f
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-04 22:41:49 +0530

    switch case condition

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-// If  else 
-//Conditional Statements (two Types)
-//if else condition
-//Switch case condition
+// Switch case condition
 
-// 1 to 6 is Good morning , 10 to 17 is Good Afternoon, 18 to 24 is Goog Evening
+let amount = 6000;
 
-let hour = new Date().getHours;
-
-if (hour >= 1 && hour <= 9) {
-    console.log("Good morning")
-} else if (hour >= 10 && hour <= 17) {
-    console.log("Good AfterNoon")
-} else {
-    console.log("Good Evening")
+switch (true) {
+  case amount >= 60000:
+    console.log("I phone ");
+    break;
+  case amount >=10000:
+    console.log("Android MObile");
+    break;
+  
+  case amount >= 5000 && amount < 10000:
+    console.log("Basic mobile");
+  default:
+    console.log("You can't afford mobile phone now ");
 }
-
-
