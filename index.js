957eb40ba4fef060c3c813dea370240315fd608d
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-05 23:01:17 +0530

    constructor Propeerty

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// Dynamic Obejects 
+// constructor Property
+/* 
+let name = "anbu"; // -> new String ("Anbu")
+let age = 24; // NumberLiterals -> new NUmber ()
+let isAlive = true; // BooleanLiterals -> new Boolean()
+ 
+console.log(name , age , isAlive);
+ */
 
-const person = {
-    name: "raj",
-    
-}
-person.age = 24;
-person.gender = "male"
-person.greeting - function () { }
-delete person.greeting;
-delete person.gender;
-console.log(person);
+let name = new String("Anbu");
+let age = new Number(3);
+let isAlive = new  Boolean(true);
+console.log(name, age, isAlive);
