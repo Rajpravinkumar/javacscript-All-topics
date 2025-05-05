e1de0e40f10ca5f92e0bf6cb2eb5f71467cf96dc
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-05 12:12:48 +0530

    for in loop

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-// do while loop 
-let i = 0;
-do {
-    if (i % 2 !== 0) {
-        console.log("Odd number D0-while #" + i);
-    }
-i++
-} while (i <= 10  )
+// for - in 
+//use in object 
+const person = {
+    name: "anbu",
+    age: 24,
+    sex: "male"
+
+}
+
+for (let key in person) {
+    console.log(key + " :" , person[key]);
+    
+}
+// use in array 
+let array = ["red", "blue", "green "]
+
+for (let key in array) {
+    console.log(array[key])
+}
