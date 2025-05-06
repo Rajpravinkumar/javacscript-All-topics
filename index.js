7c1f602d654b1fa4c79cd63d5ced929066f55430
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-06 12:43:17 +0530

    string obejct

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-// Math Object / Inbuild Objects 
+// String Objects 
+let firstName = "Anbu";
+let lastName = "selvan";
 
-let marks = [55, 65, 75, 85, 95];
-let result = Math.max(...marks);
+let firstName2 = new String("Anbu");
 
-console.log("Maximum value : " +  result);
+console.log(firstName);
+console.log(lastName);
+//properties 
+console.log(firstName.length);
 
-console.log(Math.abs(-3));
-console.log(Math.round(2.4));
-console.log(Math.ceil(2.9));
-console.log(Math.PI / 120);
-console.log(Math.SQRT2);
-console.log(Math.floor(2.9));
+//methods 
+console.log(firstName.concat(" ", lastName));
+console.log(firstName.includes("bu"));
+console.log(firstName.includes("an"));
+console.log(firstName.startsWith("an"));
+console.log(firstName.startsWith("An"));
+console.log(firstName.endsWith("bu"))
+console.log(firstName.endsWith("u"));
+console.log(firstName.indexOf("b"));
+console.log(firstName.repeat(10));
+console.log(firstName.replace("Anbu", "Rocky"));
+console.log(firstName.slice(2));
 
-//console.log(Math.random());
+
+let para = "some text";
+console.log(para.split(" ", 2));
+
+console.log(para.substring(5));
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+
+let message = " This is some text you doing good!  ";
+console.log(message.trim());
+console.log(message.trimStart());
+console.log(message.trimEnd());
+
+
+//Escap Notation 
+let hello_message = "This is my Fifth\" message, please replay."
 
 
 
 
-console.log(Math.random() * (100 -1 ) + 1);
+
+
+
