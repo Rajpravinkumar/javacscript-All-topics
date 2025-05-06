0543fec5e9a078ab502e04f7f717437a60553b6a
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-06 13:08:39 +0530

    template literials

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
-// String Objects 
-let firstName = "Anbu";
-let lastName = "selvan";
+// Template Literials 
+// string primitives 
+const name = "Trisha";
+//old type
+const message = "Thank you" + name + 'for reading \n"programing" ';
+// new types  template literials
+const message2 = `Hello ${name} , Thank you for reading this "programing" `
 
-let firstName2 = new String("Anbu");
-
-console.log(firstName);
-console.log(lastName);
-//properties 
-console.log(firstName.length);
-
-//methods 
-console.log(firstName.concat(" ", lastName));
-console.log(firstName.includes("bu"));
-console.log(firstName.includes("an"));
-console.log(firstName.startsWith("an"));
-console.log(firstName.startsWith("An"));
-console.log(firstName.endsWith("bu"))
-console.log(firstName.endsWith("u"));
-console.log(firstName.indexOf("b"));
-console.log(firstName.repeat(10));
-console.log(firstName.replace("Anbu", "Rocky"));
-console.log(firstName.slice(2));
-
-
-let para = "some text";
-console.log(para.split(" ", 2));
-
-console.log(para.substring(5));
-console.log(firstName.toLowerCase());
-console.log(firstName.toUpperCase());
-
-let message = " This is some text you doing good!  ";
-console.log(message.trim());
-console.log(message.trimStart());
-console.log(message.trimEnd());
-
-
-//Escap Notation 
-let hello_message = "This is my Fifth\" message, please replay."
-
-
-
-
-
-
-
+console.log(message)
+console.log(message2);
