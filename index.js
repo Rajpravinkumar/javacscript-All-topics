7d07871dca5f426120cd2eb932fea3e23be6248c
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-06 12:04:40 +0530

    msth / inbuild objects

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// Cloning an JavaScript Object 
+// Math Object / Inbuild Objects 
 
-let user = {
-    name: "anbu ",
-    getFullName() {
-        console.log(`My name is ${this.name}`)
-    }
+let marks = [55, 65, 75, 85, 95];
+let result = Math.max(...marks);
 
-}
-//Spread operators 
-let anotherName = {...user}
-/* let anotherName = Object.assign({age : 23}, user);
-console.log(anotherName);
- */
-console.log(anotherName)
+console.log("Maximum value : " +  result);
+
+console.log(Math.abs(-3));
+console.log(Math.round(2.4));
+console.log(Math.ceil(2.9));
+console.log(Math.PI / 120);
+console.log(Math.SQRT2);
+console.log(Math.floor(2.9));
+
+//console.log(Math.random());
+
+
+
+
+console.log(Math.random() * (100 -1 ) + 1);
