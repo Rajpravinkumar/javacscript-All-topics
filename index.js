6d630424b2fd64f139ad87f18a7353344475520f
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-10 00:10:03 +0530

    references

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-// Finding  element to an JavaScript Array (Primitives)
+// Finding  element to an JavaScript Array (references)
 
-const items = ["apple ", "Lemon", "Grapes"];
 
-console.log(items.includes('Lemon'));
+
+const orders = [
+    
+    {id:1 , item:"Apple", quantity: 3},
+    {id:2, item:"Carrot", quantity: 6},
+    {id:3 , item:"onion", quantity: 10},
+]
+
+let result = orders.find(function (order) {
+   return order.item === "Carrot"
+}) 
+
+console.log(result);
