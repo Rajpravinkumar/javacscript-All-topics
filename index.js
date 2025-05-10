5ac6ea5f38ea45e3476da444573240259c6f7fd4
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-10 22:42:03 +0530

    Emptying array

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-// Removing an Elemrnt in Array 
+// Emptying an javaScript Array 
 
-const number = [1, 2, 3, 4];
+let  numbers = [1, 2, 3, 4,];
 
-//push(end) - (pop) , 
-number.pop()
-//unshift(start) - shift
-number.shift()
-//middile(splice)
-number.splice(1,2)
+let another_numbers = numbers;
+//solution 1 
+numbers = [];
 
-console.log(number);
+//solution 2 
+numbers.length = 0;
+
+//solution 3 
+numbers.splice(0, numbers.length);
+
+//solution 4 
+while (numbers.length) numbers.pop();
+
+console.log(numbers);
