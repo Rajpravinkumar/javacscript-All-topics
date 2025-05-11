1e46a15b902c081ac3c2f5c7e9fc6fb3da8ce08b
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-11 14:54:57 +0530

    spread opeartor

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// Combining & Etracting elements Array 
-
+// Spread operator
 const shopping_cart = [ {item: "Magi"}];
 const additional_cart = ["Macthbox", "Salt", "Beans", "Masala"];
-// Combine
-let recipe = shopping_cart.concat(additional_cart);
+// spread opeartor(...)
+let recipe = [...shopping_cart, 45 , {name:"raj"}, ...additional_cart]
 shopping_cart[0].item = "Noodles";
 
-// Extract
-let extractedInfo = recipe.slice(1, 3);
 
-console.log(extractedInfo);
+
 
 console.log(recipe);
