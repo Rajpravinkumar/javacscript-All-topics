ab7254a0184e5a48bcc607dc960156bd9cabcf0d
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-13 12:18:40 +0530

    chaining methiods

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-// Mapping a javascript Array
+// JavaScript Chaining Methods
 
-const pepole = [
-  { id: 1, first_name: "Ajith", last_name: "kumar" },
-  { id: 2, first_name: "Anbu", last_name: "Selvan" },
-  { id: 3, first_name: "Trisha", last_name: "Grace" },
-  { id: 4, first_name: "Seetha", last_name: "Raman" },
+let products = [
+  { id: 1, tittle: "Android Phone", cost: 7500 },
+  { id: 2, tittle: "Gaming Computer", cost: 90500 },
+  { id: 3, tittle: "HeadPhone", cost: 2400 },
 ];
 
-let final = pepole.map((val) => {
-    let fullName = [val.id , val.first_name, val.last_name].join(" ");
-    let obj = { id: val.id , fullName}
-    return obj;
-});
+//sorting it using lowest price.
+let final = products.sort((a, b) => {
+    // a - b (ascending) ,  b - a (descending)
+    return a.cost - b.cost;
+}).sort((a, b) => {
+    // a < b = -1
+    if (a.tittle < b.tittle) return -1;
+
+    // a > b = 1
+    if (a.tittle > b.tittle) return 1;
+
+    return 1;
+})
+.filter((val) => val.cost <= 8000) 
+.map( (val) =>  val.tittle + " - $" + val.cost);
+        
+// map it like this : // Android phone -7500 
 console.log(final);
+
