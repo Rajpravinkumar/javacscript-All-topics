3d27b9041794e670c5a77f84f57e49886ad5358d
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-13 21:46:15 +0530

    q2

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 // Javascript Arrays Interview
-// -> 1) Create an Array object
-//method 1 
-var products = ["Apple", "Notebook"];
-console.log(products); // (2) ['Apple', 'Notebook']
-//method 2
-var msgArray = [];
-msgArray[0] = "Hello";
-console.log(msgArray); //['Hello']
-//method 3
-var array = new Array("Hello");
-console.log(array); // ['Hello']
-//method 4 
-var another = Array.of(1, 2, 3);
-console.log(another); // (3) [1, 2, 3]
+// -> 2) Take the below array and copy it using the slice method and the for loop method.
+
+var array = [1, 2, 3, 4, 5];
+//slice
+let result = array.slice();
+console.log(result);
+
+// for lop method
+
+let tempArray = [];
+for (let i = 0; i < array.length; i++) {
+    tempArray[i] = array[i];
+}
+
+console.log(tempArray);
+
+// spread operators 
+
+let spread = [...array]
+console.log(spread);
+
