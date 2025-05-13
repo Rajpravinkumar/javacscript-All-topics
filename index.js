a553186d6d0c9e0bd941ec31df0e029cf3df0581
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-13 22:58:53 +0530

    q6

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 // Javascript Arrays Interview
-// -> 5) How can you check if something is an Array?
-
-
+// -> 6)Add an item to the end of an array.
 
 var array = [1, 2, 3, 4, 5];
-//method 1 
-let obj = [{ id: 1 }];
-let other = undefined;
-let string = "Anbu";
-
-let result = Array.isArray(obj);
-console.log(result);
+//method 1
+array.push("Apple")
+console.log(array);
 
 // method 2 
-function checkIfArray(val) {
-    if (typeof val == "string") {
-         console.log("It is a an string ");
-         
-    } else if (typeof val == "number") {
-        console.log("It is an number");
-        
-    } else if (typeof val == "undefined") {
-        console.log("It is an undefined ");
-        
-    } else if (typeof val == "object") {
-        console.log("It is an object ");
-    }
-        
-    else {
-        console.log("It is an array");
-        
-    }
-}
-checkIfArray(other);
+array[array.length] = "Biscuit"
+console.log(array);
