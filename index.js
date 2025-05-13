dfb174081d77b4eb7df5526f3409e4d7bab263fa
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-13 22:31:42 +0530

    q4

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 // Javascript Arrays Interview
-// -> 3) Empty this array:
+// -> 4) What type is an Array set to?
 var array = [1, 2, 3, 4, 5];
-//method 1
-array = [];
 
-//method 2
-array.length = 0;
- //method 3
-while (array.length) {
-  array.pop();
-}
-
-//method 4 
-array.splice(0, array.length);
-
-console.log(array);
+console.log(typeof array);
