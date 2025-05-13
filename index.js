0aca1bac9df4685c532669c889adcbc8c5bd4815
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-13 07:58:20 +0530

    testing element in array (reference)

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-// Primitives Testing Java Script Array
-
-//Testing is it Odd or Even 
-
-const numbers = [1, 2, 3, 4, 5];
-
-// two type of method (every() , some())
-
-let isEven = numbers.every(function (value){
-
-    return value % 2 === 0;
+// Testing elements in javascript Array (Reference types )
+const items = [
+    { id: 1, name: "Mobiles", isDeliverable: true },
+    { id: 2, name: "Accessories", isDeliverable: true },
+    { id: 3, name: "Laptop", isDeliverable: false },
+    { id: 4, name: "AC", isDeliverable: true },
+    { id: 5, name: "T.V", isDeliverable: false },
+    
+]
+ //every () - method
+let allDeliverables = items.every(function (value) {
+    return value.isDeliverable == true;
 });
-console.log("isEven:", isEven);
 
-let isOdd = numbers.some(function (value) {
-    return value % 2 === 1;
+console.log(allDeliverables);
+
+// some () - method  
+let notDeliverables = items.some(function (value) {
+  return value.isDeliverable == false;
 });
-console.log("isOdd:" , isOdd);
+
+console.log(notDeliverables);
