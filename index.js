219452f0cbd7c434cdfdf97d94b618fd3eff26e6
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-13 10:09:30 +0530

    find & findindex method in array

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // filtering a javascript Array 
 
 const cart_items = [
-    { id: 1, items: "Android Mobile", cost: "6500" },
-    { id: 2, items: "I Phone Mobile", cost: "25000" },
-    { id: 3, items: "Windows Mobile", cost: "8500" },
+    { id: 1, items: "Android Mobile", cost: 6500 },
+    { id: 2, items: "I Phone Mobile", cost: 25000 },
+    { id: 3, items: "Windows Mobile", cost: 8500 },
     
 ]
 
-let affortable_phones = cart_items.filter((Value) => Value.cost < 10000)
+let affortable_phones = cart_items.findIndex((Value) => Value.cost > 10000)
+//let affortable_phones = cart_items.find((Value) => Value.cost > 10000); 
 console.log("Affortable Phone:" , affortable_phones);
