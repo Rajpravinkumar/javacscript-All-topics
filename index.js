c3ee1868c970ea0ba57ff33400ae8befa1350fdd
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-13 22:37:12 +0530

    q5

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Javascript Arrays Interview
-// -> 4) What type is an Array set to?
+// -> 5) How can you check if something is an Array?
+
 var array = [1, 2, 3, 4, 5];
 
-console.log(typeof array);
+let obj = [{ id: 1 }];
+let other = undefined;
+let string = "Anbu";
+
+let result = Array.isArray(obj);
+console.log(result);
