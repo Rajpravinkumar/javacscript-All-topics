dce779cc3a43213f72b5b0373c222105d4529148
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-14 22:38:39 +0530

    constructor

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-//Inheritance 
+// constructor
+let name = String("Anbu")
+//let name = "Anbu"
+
+console.log(name);
+
+// with classes 
 class Animal {
+    constructor(type) {
+        this.type = type;
+    }
+}
+let a = new Animal("Dog");
+console.log(a.constructor === Animal);
+
+
+// Example 
+class Person {
     constructor(name) {
         this.name = name;
-    
-    }
-
-
-    speak() {
-        console.log(`${this.name} makes a sound`);
-    
     }
 }
 
-class Dog extends Animal {
-    speak() {
-        console.log(`${this.name} barks.`);
-        
-    }
-}
+let p = new Person("Alice");
+
+console.log(p.constructor === Person); 
+
+let p2 = new p.constructor("Bob");
+console.log(p2.name);
+
