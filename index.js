88fc43022f14e45632d61ccd7157e1b83fae4670
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-05-16 15:05:23 +0530

    string object

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-// Math Object in js provides built-in mathematical constant & function - No need to create a new instance ('Its static)
+//String object represent and manipulate a sequence of character , strings can be primitive or objects , but js automatically convert between them when needed
 
-/* 1. 
-Common Math Properties
-Property | 	Description	 | Value
-Math.PI  |	π (Pi)	|3.14159...
-Math.E	Euler's number	2.71828...
-Math.LN2	Natural log of 2	0.693...
-Math.SQRT2	Square root of 2	1.414... */
+let str = "Hello World"; //string
 
-/* 2. 
-Useful Math Methods
+let strObj = new String("Hello World!") //object
 
-Method	Description	Example
-Math.round(x)	Rounds to nearest integer	Math.round(4.7) → 5
-Math.floor(x)	Rounds down to nearest integer	Math.floor(4.9) → 4
-Math.ceil(x)	Rounds up to nearest integer	Math.ceil(4.1) → 5
-Math.trunc(x)	Removes decimal part	Math.trunc(4.9) → 4 */
+let message = "Hello , JavaScript";
 
-/*
- Arithmetic & Roots
-Method	Description	Example
-Math.pow(x, y)	x to the power of y	Math.pow(2, 3) → 8
-Math.sqrt(x)	Square root of x	Math.sqrt(16) → 4
-Math.abs(x)	Absolute value	Math.abs(-7) → 7 */
+//length 
+message.length; // 18
 
-/* 
-Random & Min/Max
-Method	Description	Example
-Math.random()	Random number between 0 and 1	Math.random() → 0.28 (example)
-Math.min(...x)	Minimum of values	Math.min(3, 1, 4) → 1
-Math.max(...x)	Maximum of values	Math.max(3, 1, 4) → 4 */
-let x = 7;
-let y = 2;
-let math = Math.abs(x);
-console.log(math);
+//Access Characters
+message.charAt(0);
+message[0];
 
-//Random interger Between Two Values
+// Case conversion
+message.toUpperCase();
+message.toLowerCase();
+//search 
+message.indexOf("java")
+message.includes("Java");
+//Extraxt substrings
+message.slice(7, 12);
+message.substring(7, 12);
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1 )) ;
-}
-console.log(getRandomInt(1,10));
+//Replace
+message.replace("JavaScript", "JS!")
+
+//split 
+message.split(" , ");
+
+//Trim WhiteSpace 
+let name = "   John Doe  "
+let trimmed = name.trim();
+console.log(trimmed);
+console.log(name.length); //13
+console.log(trimmed.length); // 8
+
+
+//Strinh Object Vs Primitive
+
+let primitive = "abc"; //string
+let non_primitive = new String("abc"); //object
+
+console.log(primitive === non_primitive); //false
+console.log(primitive == non_primitive); //true
+
+
+
